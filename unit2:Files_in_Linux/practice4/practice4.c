f59a8b825be86acb7ba5e27fb9cac21874ced763
Nam Khuc <nam.khuc@nxp.com>
2018-12-20 15:59:22 +0700

    add practice4 for unit2

--- unit2:Files_in_Linux/practice4/practice4.c
+++ unit2:Files_in_Linux/practice4/practice4.c
@@ -1,24 +1,70 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 int main ( int argc, char *path[] )
 {
     char homeDir[1000] = {0};
-    int mode = 0;
+    char cmdTerminal[1000] = {0};
 
-    // Copy path to homeDir
-    strcpy (homeDir, path[1]);
+    // printf ("%s\n", path[0]);
+    // printf ("%s\n", path[1]);
 
-    // create 'Test folder'
-    strcat (homeDir, "Test_folder");
-    mode = 0777;
-    mkdir (homeDir,0777);
+    if (sizeof(path[1]) > 0)
+    {
+        // Copy path to homeDir
+        strcpy (homeDir, path[1]);
 
-    // create 'Test1' folder
-    // strcat (homeDir, "/Test1");
-    // mkdir (homeDir, 664);
+        // create 'Test folder'
+        strcat (homeDir, "Test_folder");
+        mkdir (homeDir, 0764);
 
-    return 0;
+        // create 'efg' folder
+        strcat (homeDir, "/efg");
+        mkdir (homeDir, 0664);
+
+        // create the 'Test1' folder
+        // reset hoemDir
+        strcpy (homeDir, path[1]);
+        // make path for Test1 folder
+        strcat (homeDir, "Test_folder/Test1");
+        mkdir (homeDir, 0764);
+
+        // create the abc folder
+        strcat (homeDir, "/abc");
+        mkdir (homeDir, 0444);
+
+        // change permision for Test1
+        // reset hoemDir
+        strcpy (homeDir, path[1]);
+        // make path for Test1 folder
+        strcat (homeDir, "Test_folder/Test1");
+        strcpy (cmdTerminal, "chmod 664 ");
+        strcat (cmdTerminal, homeDir);
+        // printf ("%s\n", cmdTerminal);
+        system (cmdTerminal);
+
+        // reset cmd buffer
+        strcpy (cmdTerminal, "");
+
+        // change permision for Test_folder
+        // reset hoemDir
+        strcpy (homeDir, path[1]);
+        // make path for Test_folder
+        strcat (homeDir, "Test_folder");
+        strcpy (cmdTerminal, "chmod 664 ");
+        strcat (cmdTerminal, homeDir);
+        // printf ("%s\n", cmdTerminal);
+        system (cmdTerminal);  
+
+        return 0;
+    }
+    else
+    {
+        printf ("Error: Missing argument!");
+
+        return 1;
+    }
 }
