9c7b8d86df51a75323c14db5f1b7d44d9e71031c
root <NamKD>
2019-01-12 03:03:03 -0800

    Add practice2 for unit5

--- unit5:signal/practice2/wakeup.c
+++ unit5:signal/practice2/wakeup.c
@@ -1,1 +1,50 @@
-ps --no-header -o pid -C "prac2" >> Data.txt
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h> 
+#include <signal.h>
+
+int main()
+{
+    int error = 0;
+    int fd = 0;
+    char buf [10000];
+    int read_rel = 0;
+    char cmd [10000];
+    int index = 0;
+
+
+    system("ps --no-header -o pid -C 'prac2' >> ./proceId.txt");
+
+    fd = open("./proceId.txt",O_RDONLY);
+    if ( fd > 0)
+    {
+        printf ("open and read proceId.\n");
+        read_rel = read (fd, buf, 10000);
+        if (read_rel > 0)
+        {
+            strcpy (cmd, "");
+            index = sprintf (cmd, "kill ");
+            index += sprintf (cmd + index, "-12");
+            index += sprintf (cmd + index, "%s", buf);
+            printf ("%s", cmd);
+            system (cmd);
+        }
+        else
+        {
+            printf ("error!\n");
+        }
+        close(fd);
+    }
+    else
+    {
+        printf("Error during open proceId file!\n");
+        error = 1;
+    }
+
+
+
+    return (error); 
+}
+
