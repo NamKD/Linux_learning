39b9c5348a70b803eb80ea0c549f271fca50f6da
Nam Khuc <kdnbkhn@gmail.com>
2019-01-31 22:27:00 +0700

    add final version for unit6/practice4

--- unit6:multithread/practice4/prac4.c
+++ unit6:multithread/practice4/prac4.c
@@ -3,14 +3,13 @@
 #include <pthread.h>
 
 extern void file_write (char *string);
-int ind = 0;
 
 void *sub_thread (void *argu)
 {
     char str[1000] = {0};
-    ind = 50;
+    int b = 50;
 
-    sprintf (str, "The sub thread is: %d", ind);
+    sprintf (str, "The sub thread is: %d\n", b);
     file_write(str);
 
     return 0;
@@ -20,11 +19,11 @@ int main (void)
 {
     pthread_t tid;
     char str[1000] = {0};
-    ind = 100;
+    int a = 100;
 
     pthread_create (&tid, NULL, &sub_thread, NULL);
     pthread_join (tid, NULL);
-    sprintf (str, "The main thread is: %d", ind);
+    sprintf (str, "The main thread is: %d\n", a);
     file_write(str);
 
     return 0;
--- unit6:multithread/practice4/wrapper.c
+++ unit6:multithread/practice4/wrapper.c
@@ -7,11 +7,11 @@ FILE *file;
 void file_write (char *string)
 {
     pthread_mutex_t mutex_my_file;
-
+    
     pthread_mutex_init (&mutex_my_file, NULL);
     
     pthread_mutex_lock (&mutex_my_file);
-    file = fopen ("./namkd.txt","w");
+    file = fopen ("./namkd.txt","a");
     fwrite (string, 1, strlen(string), file);
     fclose (file);
     pthread_mutex_unlock (&mutex_my_file);
